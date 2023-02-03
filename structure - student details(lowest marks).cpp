0858edb7ef427620fa50c7eafbbd55826004b523
Vanshp2104 <112387485+Vanshp2104@users.noreply.github.com>
2023-02-03 16:18:18 +0530

    Add files via upload

---
+++ structure - student details(lowest marks).cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+struct ndt{
+	int roll;
+	char name[20];
+	int marks;
+};
+typedef struct ndt student;
+main()
+{
+	student st1[10];
+	for(int i=0;i<3;i++)
+	{
+	printf("Enter roll no");
+	scanf("%d",&st1[i].roll);
+	printf("Enter Name");
+	scanf("%s",&st1[i].name);
+	printf("Enter marks");
+	scanf("%d",&st1[i].marks);
+	}
+	for(int i=0;i<3;i++)
+	{
+		printf("Roll no : %d Name : %s Marks : %d \n",st1[i].roll,st1[i].name,st1[i].marks);
+	}
+	int max,j,k;
+	for(int i=0;i<3;i++)
+	{
+		if (max<st1[i].marks)
+		{
+			max=st1[i].marks;
+			j=i;
+		}
+	}
+	for(int i=0;i<3;i++)
+	{
+		if (max>st1[i].marks)
+		{
+			max=st1[i].marks;
+			k=i;
+		}
+	}
+	printf("Highest Marks %s\n",st1[j].name);
+	printf("Lowest Marks %s",st1[k].name);
+	
+}
+
